a84f44e5c6d72aeef77e68809c90a82772b2d8c5
HeenaNautiyal <heenanautiyalcse@gmail.com>
2017-04-24 13:09:13 +0530

    main/

---
+++ Config.java
@@ -0,0 +1,11 @@
+package com.heena.myimage;
+
+/**
+ * Created by Heena on 2/17/2017.
+ */
+public class Config {
+    public static final String FILE_UPLOAD_URL = "http://d2dmedicine.com/aPPmob_lie/insertdata.php?caseid=10";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
